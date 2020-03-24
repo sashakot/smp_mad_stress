3edd5857773151c7b7942874d48cca9fd1d6f4ca
Sasha Kotchubievsky <sashakot@mellanox.com>
2020-03-24 23:44:28 +0200

    23March
    Signed-off-by: Sasha Kotchubievsky <sashakot@mellanox.com>

--- test.sh
+++ test.sh
@@ -4,19 +4,19 @@
 LID=${LID:="10,8,16,4,11,3,15,9"}
 #ATTR=${ATTR:="0x10 0x11 0x12 0x14 0x15 0x16 0x17 0x18 0x19 0x1A 0x1B 0x33"}
 #ATTR=${ATTR:="0x19"}
-ATTR=${ATTR:="0x15"}
+ATTR=${ATTR:="0xff22"}
 
 echo $LID
 
-for method in {1..1}; do
+for method in {2..2}; do
 	echo "Method: $method"
 	for attr in $ATTR; do
 		echo "Attr: $attr"
-		for n in {8..8}; do
-			echo "n: $n"
-			./smp_mad_stress -C mlx5_3  -m $method -N 128 -n $n  -t 20 -L $LID $attr 1  1 > ./out1.log &2>1 &
-			./smp_mad_stress -C mlx5_3  -m $method -N 128 -n $n  -t 20 -L $LID $attr 1  1 > ./out2.log &2>1 &
-			./smp_mad_stress -C mlx5_3  -m $method -N 128 -n $n  -t 20 -L $LID $attr 1  1 > ./out3.log &2>1
+		for N in {64..256..32}; do
+			for n in {1..64}; do
+				echo "n: $n"
+				./smp_mad_stress -C mlx5_2  -p 3 -m $method -N 128  -n $n  -t 10 -L 123,104,34,79,80,133,21,31,26,30,36,43,9,152,157 0xff22 1  1
+			done
 		done
 	done
 done
